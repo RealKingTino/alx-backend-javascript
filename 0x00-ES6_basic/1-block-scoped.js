b1a63027d55ba4952712eb4097c09bc78413331b
RealKingtino <tmowizzy@gmail.com>
2024-01-03 00:27:24 -0800

    added comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,8 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
+  /* Use let to avoid overwriting the outer variables */
   if (trueOrFalse) {
     let task = true;
     let task2 = false;
   }
-
-  return [task, task2];
-}
