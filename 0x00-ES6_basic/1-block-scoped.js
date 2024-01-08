091b7781a506f27a1648833b4c2064a3a3a66707
Aliyu <122824528+RealKingTino@users.noreply.github.com>
2024-01-08 13:08:32 +0100

    1-main.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      /* eslint-disable no-unused-vars */
-      const task = true;
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // Use let or const here instead of var
+    const task = true;
+    const task2 = false;
   }
+
+  return [task, task2];
+}
