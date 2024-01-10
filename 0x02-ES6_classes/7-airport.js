665b2ad8a3eed8490cf83b768eec12806eedf0a8
RealKingtino <tmowizzy@gmail.com>
2024-01-10 10:23:53 -0800

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,37 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = '';
-    this._code = '';
-
-    this.name = name;
-    this.code = code;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new Error('Name must be a string');
-    }
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(newCode) {
-    if (typeof newCode === 'string') {
-      this._code = newCode;
-    } else {
-      throw new Error('Code must be a string');
-    }
+    this._name = name;
+    this._code = code;
   }
 
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
