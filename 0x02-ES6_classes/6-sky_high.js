e0ec93e2317b0ce025edf17e9f6a1ebc38e74f19
RealKingtino <tmowizzy@gmail.com>
2024-01-10 10:26:32 -0800

    6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,25 +1,16 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = 0;
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors === 'number') {
-      this._floors = newFloors;
-    } else {
-      throw new Error('Number of floors must be a number');
-    }
-  }
-
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
