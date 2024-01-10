7f100d18e6891d8ac0ad2ecc0f5d644a745211a8
RealKingtino <tmowizzy@gmail.com>
2024-01-10 10:28:13 -0800

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = 0;
-    this.sqft = sqft;
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(newSqft) {
-    if (typeof newSqft === 'number') {
-      this._sqft = newSqft;
-    } else {
-      throw new Error('Square footage must be a number');
-    }
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
