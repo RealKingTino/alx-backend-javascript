9d597d8ab5ab78c01b8ef46a54146eb7f0f32924
RealKingtino <tmowizzy@gmail.com>
2024-01-13 01:38:30 -0800

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw Error("Position outside range");
+    throw Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer, 0, length);
